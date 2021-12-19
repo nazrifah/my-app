f8dc0d3f4037ba1226c65656e1fcd8d39dc2ddba
Nazri <nazrilawliet@gmail.com>
2021-12-20 01:47:50 +0700

    menambahkan string di OptionActivity.kt

--- app/src/main/java/com/example/my_application/OptionActivity.kt
+++ app/src/main/java/com/example/my_application/OptionActivity.kt
@@ -7,5 +7,7 @@ class OptionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_option)
+
+        println("hello nazri")
     }
 }
